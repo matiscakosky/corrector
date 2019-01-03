1e43386d71f78b70d1665e9b9b527b28d83b8327
matiscakosky <scakosky@hotmail.com>
2019-01-02 23:38:01 -0300

    primer prueba de API Google Spreadsheets, subo el quickstart

---
+++ alumnos/quickstart.py
@@ -0,0 +1,12 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope=['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name("My Project-150473bd33ba.json",scope)
+
+gc=gspread.authorize(credentials)
+
+wks=gc.open('Alumnos').sheet1
+
+print(wks.append_row(["Muller Thomas","A"]))
