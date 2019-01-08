090cd46df0d819947ff073aa94d2370e3ac0bab8
matiscakosky <scakosky@hotmail.com>
2019-01-08 18:03:06 -0300

    agrego documentacion: sacar lo de las 23:59:59 que pasa si es un examen e importa la hora

--- scripts/vencimientos.py
+++ scripts/vencimientos.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Script que contiene la clase LimiteDeEntrega. Encargada de manipular las credenciales de los trabajos practicos y decidir si un trabajo practico
-- Se corrige normalmente
-- Se corrige con advertencia por entrega tardía 
-- No se corrige
+Script que contiene la clase LimiteDeEntrega. Encargada de manipular las credenciales de los trabajos practicos y decidir que hacer dadod un trabajo practico
+NO ES SENSIBLE A FALLOS. SI NO EXISTEN LAS CREDENCIALES DEBE SEGUIR CORRIGIENDO NORMALMENTE
+
+La fecha de vencimiento sera dada en clase y tendran tiempo de hacerla hasta ese dia a las 23:59:59
 
 
-NO DEBE SER SENSIBLE A FALLOS. SI NO EXISTEN LAS CREDENCIALES DEBE SEGUIR CORRIGIENDO NORMALMENTE
 """
 from excepciones import TrabajoVencido
 import json
 from datetime import datetime
 
 class LimiteDeEntrega:
+    """La clase modela un limitador que dice si el tp esta en condiciones de ser corregido o no.
+    La clase se genera a partir del path del trabajo practico a delimitar y la fecha en la que el trabajo fue entregado.
+    Un archivo de credenciales en formato json debe estar dentro del path de pruebas del trabajo practico junto con todo lo demás que haga falta para que sea corregido
+    La clase LimiteDeEntrega leera previamente el archivo de crendenciales y guardara entre sus atributos si debe enviar una advertencia al alumno por el vencimiento o directamente lanza
+    una excepcion al flujo principal porque el trabajo esta vencido.
+    """
     
     def __init__(self, skel_dir, fechaAlumno):
         self.credential=skel_dir / "credentials.json"
@@ -20,6 +25,11 @@ class LimiteDeEntrega:
         self.advertencia = False
     
     def confirmar_horario_entrega(self):
+        """Confirma segun las credenciales almacenadas en sus atributos si:
+        - Se corrige normalmente
+        - Se corrige con advertencia por entrega tardía 
+        - No se corrige:
+        """
         credential={}
         try:
             with open(self.credential) as handle:
