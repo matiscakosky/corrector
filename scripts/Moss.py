cb10b00e8458b5b1804b41f0781789ba5244bb0a
matiscakosky <scakosky@hotmail.com>
2019-02-05 12:06:58 -0300

    cambios en pedido de contraseña

--- scripts/Moss.py
+++ scripts/Moss.py
@@ -28,7 +28,9 @@ class Moss:
     
     def subir(self):
         """"askpass.py es un script que saque de internet para obtener facil la contraseña de github"""
-        project_dir = os.path.dirname(os.path.abspath(__file__))
+        
+		#Descomentar si no tengo la contrasenia guardada
+		#project_dir = os.path.dirname(os.path.abspath(__file__))
         #os.environ['GIT_ASKPASS'] = os.path.join(project_dir, 'askpass.py')
         #os.environ['GIT_USERNAME'] = "matiscakosky"
         #os.environ['GIT_PASSWORD'] = os.environ["PASS"]
