9a0fdb6820f5b05ffcf05f3eba02714e5e824f53
matiscakosky <scakosky@hotmail.com>
2019-02-18 13:57:27 -0300

    finalizo test de Vector

--- SKEL_TP/VECTOR/Tests.java
+++ SKEL_TP/VECTOR/Tests.java
@@ -12,7 +12,7 @@ public class Tests {
     public void test01CrearDosVectoresIgualesDebenSerElMismo(){
         Vector vector1 = new Vector(1,2);
         Vector vector2 = new Vector(1,2);
-        print_test("CrearDosVectoresIgualesDebenSerElMismo",vector1.esIgual(vector2));
+        print_test("Crear dis vectiores iguales y deben ser el mismo",vector1.esIgual(vector2));
         assertTrue(vector1.esIgual(vector2));
     }
 
@@ -22,43 +22,43 @@ public class Tests {
         Vector vector2 = new Vector(3,-5);
         Vector vectorSum = vector1.sumar(vector2);
         Vector vecTest = new Vector(4,-3);
-        print_test("CrearDosVectoresYSumarlos",vecTest.esIgual(vectorSum));
+        print_test("Sumo dos vectores",vecTest.esIgual(vectorSum));
         assertTrue(vecTest.esIgual(vectorSum));
     }
-	
-	
-	@Test
+    
+    
+    @Test
     public void testCrearTresVectoresYSumarlos(){
         Vector vector1 = new Vector(1,2);
         Vector vector2 = new Vector(3,-5);
-		Vector vector3 = new Vector(4,10);
+        Vector vector3 = new Vector(4,10);
         Vector vectorSum = vector1.sumar(vector2);
-		vectorSum = vectorSum.sumar(vector3);
+        vectorSum = vectorSum.sumar(vector3);
         Vector vecTest = new Vector(8,7);
         print_test("Crear 3 vectores consecutivos y sumar",vecTest.esIgual(vectorSum));
         assertTrue(vecTest.esIgual(vectorSum));
     }
-	
-	@Test
-	public void sumarVectorNulo(){
+    
+    @Test
+    public void sumarVectorNulo(){
         Vector vector1 = new Vector(1,1);
         Vector vector2 = new Vector(0,0);
-		Vector vector3 = new Vector(0,0);
+        Vector vector3 = new Vector(0,0);
         Vector vectorSum = vector1.sumar(vector2);
-		vectorSum = vectorSum.sumar(vector3);
+        vectorSum = vectorSum.sumar(vector3);
         Vector vecTest = new Vector(1,1);
         print_test("Crear 3 vectores consecutivos y sumar",vecTest.esIgual(vectorSum));
         assertTrue(vecTest.esIgual(vectorSum));
     }
-	
-	@Test
-	public void pruebaDeVolumendeSumar(){
-		Vector v = new Vector(0,0);
-		for (int i=1;i<=1000;i++){
-			v=v.sumar(new Vector(i,i));
-		}
-		Vector vres= new Vector(500500,500500);
-		print_test("pruebaDeVolumenDeSumar",vres.esIgual(v));
+    
+    @Test
+    public void pruebaDeVolumendeSumar(){
+        Vector v = new Vector(0,0);
+        for (int i=1;i<=1000;i++){
+            v=v.sumar(new Vector(i,i));
+        }
+        Vector vres= new Vector(500500,500500);
+        print_test("Creo mil vectores y los sumo todos",vres.esIgual(v));
         assertTrue(v.esIgual(vres));
     }
 
@@ -68,7 +68,7 @@ public class Tests {
         Vector vector2 = new Vector(0,1);
         Vector vectorSum = vector1.restar(vector2);
         Vector vecTest = new Vector(0,-2);
-        print_test("Crear2YRestarVectores",vecTest.esIgual(vectorSum));
+        print_test("Prueba de restar vectores",vecTest.esIgual(vectorSum));
         assertTrue(vecTest.esIgual(vectorSum));
     }
 
@@ -77,25 +77,25 @@ public class Tests {
         Vector vector1 = new Vector(5,3);
         Vector vector2 = new Vector(-1,2);
         int RESULTADO_PI = 1;
-        print_test("Crear2yObtenerSuProductoInterno",vector1.calcularProductoInterno(vector2)==RESULTADO_PI);
+        print_test("Prueba del producto interno con positivos",vector1.calcularProductoInterno(vector2)==RESULTADO_PI);
         assertEquals(vector1.calcularProductoInterno(vector2),RESULTADO_PI);
     }
-	
-	@Test
+    
+    @Test
     public void productoInternoConCero(){
         Vector vector1 = new Vector(1,5);
         Vector vector2 = new Vector(0,0);
         int RESULTADO_PI = 0;
-        print_test("ProductoInternoConCero",vector1.calcularProductoInterno(vector2)==RESULTADO_PI);
+        print_test("Prueba del producto interno con vector nulo",vector1.calcularProductoInterno(vector2)==RESULTADO_PI);
         assertEquals(vector1.calcularProductoInterno(vector2),RESULTADO_PI);
     }
-	
-	@Test
+    
+    @Test
     public void productoInternoConNegativos(){
         Vector vector1 = new Vector(-5,-3);
         Vector vector2 = new Vector(-1,-2);
         int RESULTADO_PI = 11;
-        print_test("Crear2yObtenerSuProductoInterno",vector1.calcularProductoInterno(vector2)==RESULTADO_PI);
+        print_test("Prueba del producto interno con negativos",vector1.calcularProductoInterno(vector2)==RESULTADO_PI);
         assertEquals(vector1.calcularProductoInterno(vector2),RESULTADO_PI);
     }
 
@@ -103,7 +103,7 @@ public class Tests {
     public void test05CrearUnVectorYCaclularSuMagnitud(){
         Vector vector1 = new Vector(8,5);
         int RESULTADO_MAG = 9;
-        print_test("CrearUnVectorYCaclularSuMagnitud",vector1.calcularMagnitud()==RESULTADO_MAG);
+        print_test("Prueba de calculo de magnitud no entera debe truncar",vector1.calcularMagnitud()==RESULTADO_MAG);
         assertEquals(vector1.calcularMagnitud(),RESULTADO_MAG);
     }
 
@@ -112,61 +112,102 @@ public class Tests {
     public void test06CrearVectorYCambiarSuSentido(){
         Vector v1 = new Vector(1,-1);
         v1.cambiarSentido();
-        print_test("CrearVectorYCambiarSuSentido",v1.esIgual(new Vector(-1,1)));
+        print_test("Prueba de cambio de sentido de un vector",v1.esIgual(new Vector(-1,1)));
         assertTrue(v1.esIgual(new Vector(-1,1)));
     }
+    
+    
+    @Test
+    public void CrearVectorNuloYCambiarSuSentido(){
+        Vector v1 = new Vector(0,0);
+        v1.cambiarSentido();
+        print_test("Crear vector nulo y cambiarle el sentido",v1.esIgual(new Vector(0,0)));
+        assertTrue(v1.esIgual(new Vector(0,0)));
+    }
+    
+    
+    @Test
+    public void crearVectorNoIgualDebeFallar(){
+        Vector vector1 = new Vector(1,2);
+        Vector vector2 = new Vector(3,2);
+        print_test("Crear dos vectores diferentes para es igual, debe fallar",!vector1.esIgual(vector2));
+        assertTrue(!vector1.esIgual(vector2));
+    } 
+    
+    @Test
+    public void testVolumenParaParalelos(){
+        Vector vector1= new Vector(1,1);
+        for(int i=0;i<1000;i++){
+            Vector paralelo= new Vector(i,i);
+            if (!vector1.sonParalelos(paralelo)){
+                throw new AssertionError();
+            }
+        }
+        print_test("Prueba de parelelos en volumen", true);
+    }
+    
     @Test
     public void test07CrearVectoresYFijarseSiSonParalelos(){
         Vector v1=new Vector(1,-2);
         Vector v2=new Vector(2,-4);
-        print_test("CrearVectoresYFijarseSiSonParalelos",v1.sonParalelos(v2));
+        print_test("Prueba de paralelos",v1.sonParalelos(v2));
         assertTrue(v1.sonParalelos(v2));
     }
-	
-	@Test
+    
+    @Test
     public void test07CrearVectoresYFijarseSiSonParalelosConCerosEnSusCoordenadas(){
         Vector v1=new Vector(0,-2);
         Vector v2=new Vector(0,-4);
-        print_test("CrearVectoresYFijarseSiSonParalelosConCerosEnSusCoordenadas",v1.sonParalelos(v2));
+        print_test("Creo dos vectores y checkeo que sean paralelos, tienen un cero en sus coordenadas",v1.sonParalelos(v2));
         assertTrue(v1.sonParalelos(v2));
     }
-	
-	@Test
+    
+    @Test
     public void test09crearDosVectoresConNumerosGrandesYFijarseSiSonParalelos(){
         Vector v1=new Vector(1,8);
         Vector v2=new Vector(5,40);
         print_test("Pruebo paralelos con nuemeros grandes",v1.sonParalelos(v2));
         assertTrue(v1.sonParalelos(v2));
     }
-	
-	@Test
+    
+    @Test
     public void test07CrearVectoresYFijarseSiSonParalelosConCambioDeSigno(){
         Vector v1=new Vector(-5,2);
         Vector v2=new Vector(10,-4);
         print_test("Crear vectores y fijarse si son paralelos con cambio de signo",v1.sonParalelos(v2));
         assertTrue(v1.sonParalelos(v2));
     }
-	
-	@Test(expected = AssertionError.class)
+    
+    @Test(expected = AssertionError.class)
     public void test07CrearVectoresYFijarseSiNoSonParalelosDebeFallar(){
         Vector v1=new Vector(0,-2);
         Vector v2=new Vector(1,74);
         print_test("Crear vectores y fijarse si no son paralelos debe fallar",!v1.sonParalelos(v2));
         assertTrue(v1.sonParalelos(v2));
     }
-
+    
+    
+    @Test(expected = AssertionError.class)
+    public void pruebaRestarDosVectoresYQueNoLosSume(){
+        Vector v1=new Vector(1,1);
+        Vector v2=new Vector(1,1);
+        v2=v1.restar(v2);
+        print_test("Resto dos vectores, checkeo quen no los sume",!v2.esIgual(new Vector(2,2)));
+        assertTrue(v2.esIgual(new Vector(2,2)));
+    }
+    
+    
     @Test
     public void test08CrearVectorYDuplicarlo(){
         Vector vector1 = new Vector(10,-15);
         vector1.multplicarPorEscalar(2);
         Vector vecTest = new Vector(20,-30);
-        print_test("CrearVectorYDuplicarlo",vecTest.esIgual(vector1));
+        print_test("Creo un vector y lo multplico por 2",vecTest.esIgual(vector1));
         assertTrue(vecTest.esIgual(vector1));
     }
 
     @Test
     public void testCrearUnVectorYSuInversoSumarAmbosAUnTercerVector(){
-        /*Corregir y explicar Porque falla la prueba*/
         Vector v1 = new Vector(1,1);
         Vector v2 = new Vector(1,1);
         v2.cambiarSentido();
@@ -182,7 +223,7 @@ public class Tests {
     }
 
     @Test
-    public void integracion01(){
+    public void testCreoVectoresYOperoParaLograrUnaMagnitud0(){
         Vector vector1 = new Vector(1,2);
         vector1.multplicarPorEscalar(3);
         vector1 = vector1.sumar(new Vector(-3,-6));
@@ -192,7 +233,7 @@ public class Tests {
         assertEquals(magnitud,0);
     }
     @Test
-    public void integracion02(){
+    public void testCreoVectoresYOperoParaLograrQueSeanParalelos(){
         Vector vector1 = new Vector(3,8);
         vector1.multplicarPorEscalar(2);
         vector1 =vector1.restar(new Vector(4,14));
@@ -203,6 +244,15 @@ public class Tests {
         vector2.multplicarPorEscalar(50);
         print_test("Creo dos vectores diferentes y opero hasta que sean paralelos",vector1.sonParalelos(vector2));
         assertTrue(vector1.sonParalelos(vector2));
+    }
+    
+    @Test
+    public void testIntentoConseguirElAnguloConLasHerramientasTenidas(){
+        Vector v1 = new Vector(0,1);
+        Vector v2 = new Vector(1,0);
+        double cosTita= v1.calcularProductoInterno(v2) / v1.calcularMagnitud() * v2.calcularMagnitud();
+        int tita = (int)Math.toDegrees(Math.acos(cosTita));
+        assertEquals(90,tita);
     }
 
 
