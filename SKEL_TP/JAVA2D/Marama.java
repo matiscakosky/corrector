cc22f53059d7c2859b0deb6d34d37f726261a3c3
matiscakosky <scakosky@hotmail.com>
2020-03-13 17:02:46 -0300

    borro todo el skel 2019

--- SKEL_TP/JAVA2D/Marama.java
+++
@@ -1,8 +0,0 @@
-
-public class Marama implements Banda {
-	
-	public String reproducir(){
-		return "Suena Marama";
-	}
-
-}
--- SKEL_TP/TPPY3/testing.py
+++
@@ -1,9 +0,0 @@
-
-
-class show_test:
-    def __init__(self):
-        self.fails=0
-        
-    def print_test(self,mensaje,ok):
-        if not ok: self.fails += 1
-        print("{} ... {}".format(mensaje,"OK" if ok else "ERROR" ))
